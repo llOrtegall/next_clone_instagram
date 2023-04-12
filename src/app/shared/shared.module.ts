c4f8f8953c53d5fb027b0d5cfeca3e967bb92735
llOrTeGall <IvanOrtega@hotmail.com>
2023-04-12 04:22:14 -0500

    aplicando router Oulet

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SidebarComponent,
+  ]
+})
+export class SharedModule { }
