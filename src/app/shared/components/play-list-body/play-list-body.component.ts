6f63373c2e593519df96bf68c66e19de9955a281
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-08 08:42:11 -0500

    agregando dos componentes compartidos

---
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from "../../../data/tracks.json";
+import { TrackModel } from '@core/model/track.model';
+
+@Component({
+  selector: 'app-play-list-body',
+  templateUrl: './play-list-body.component.html',
+  styleUrls: ['./play-list-body.component.css']
+})
+export class PlayListBodyComponent  implements OnInit {
+
+  tracks: TrackModel[] = [];
+  
+  constructor(){ }
+
+  ngOnInit(): void {
+      const {data} :any = (dataRaw as any).default
+      this.tracks = data;
+  }
+
+}
