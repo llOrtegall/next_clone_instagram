bbc29d71a56ab38e1b4ef9de172f041e67527444
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-09 06:38:22 -0500

    login reactivo con validación basica implementado

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
   styleUrls: ['./auth-page.component.css'],
 })
-export class AuthPageComponent  implements OnInit {
+export class AuthPageComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
 
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {
-      
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [
+        //Aquí podemos colocar las validaciones
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  sendLogin():void {
+    const body = this.formLogin.value;
+    console.log(body);
   }
 }
