20518dd7ed9048c892c048ab6a7ecdcd6c4e4b97
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-20 19:33:32 -0500

    confirmando cambios

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -5,15 +5,12 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class ImgBrokenDirective {
   // TODO: Host hace referencia al huespe
-
   @HostListener('error') handleError():void{
     const elNative = this.elHost.nativeElement;
     elNative.src = '../../../assets/broken2.png';
     //console.log('img Broken', this.elHost)
   }
-
   constructor(private elHost: ElementRef) {
     console.log(this.elHost)
   }
-
 }
