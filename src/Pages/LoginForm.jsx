6a85049aafb575f8acfa0f962a5edd1aa23031ec
llOrtegall <IvanOrtega_97@hotmail.com>
2023-11-12 11:27:23 -0500

    cambios de maquetación listos

--- src/Pages/LoginForm.jsx
+++ src/Pages/LoginForm.jsx
@@ -4,24 +4,20 @@ import './Login.css'
 export function LoginForm () {
   return (
     <section>
-      <div className="form-box">
-        <div className="form-value">
-          <form>
-            <h2>Bienvenido</h2>
-            <div className="inputbox">
-              <figure className='icons'><User /></figure>
-              <input type="text" required />
-              <label >Username</label>
-            </div>
-            <div className="inputbox">
-              <figure className='icons'><Lock /></figure>
-              <input type="password" required />
-              <label >Password</label>
-            </div>
-            <button>Iniciar Sesion</button>
-          </form>
+      <form className='form-box'>
+        <h2>Bienvenido</h2>
+        <div className="inputbox">
+          <figure className='icons'><User /></figure>
+          <input type="text" required />
+          <label >Usuario</label>
         </div>
-      </div>
+        <div className="inputbox">
+          <figure className='icons'><Lock /></figure>
+          <input type="password" required />
+          <label >Contraseña</label>
+        </div>
+        <button>Iniciar Sesion</button>
+      </form>
     </section>
   )
 }
