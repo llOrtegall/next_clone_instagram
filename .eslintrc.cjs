8e7ef0d03f7275d06ab5f038f03d8030f5a315ed
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-01 11:20:00 -0500

    standard agregado

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'standard',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -17,5 +18,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
   },
 }
