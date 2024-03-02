64e6a220ff879cb30c09b7677501d4b6dc501c84
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-02 11:52:10 -0500

    remix retirado

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,31 @@ import { Resumen } from './pages/Resumen'
 
 export function App () {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path='/resumen' element={<Resumen />} />
-    </Routes>
+    <section className='w-screen h-screen flex'>
+
+      <nav className='bg-blue-300 w-[20vw] h-[100vh]'>
+
+        <figure>
+          <i className="ri-building-line"></i>
+        </figure>
+
+        <ul className='flex items-center flex-col'>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/resumen">Resumen</a>
+          </li>
+        </ul>
+      </nav>
+
+      <main className='w-[80vw] bg-red-200'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/resumen' element={<Resumen />} />
+        </Routes>
+      </main>
+
+    </section>
   )
 }
