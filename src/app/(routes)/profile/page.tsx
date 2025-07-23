4363eaa1059d9aaa70fe16ec406d5e8a93118609
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-07-23 17:18:19 -0500

    modify action and modify image view component

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -35,7 +35,7 @@ export default async function ProfilePage() {
               <Image
                 className=""
                 width={160} height={160}
-                src="https://plus.unsplash.com/premium_photo-1672239496290-5061cfee7ebb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src={profile.avatar || ''}
                 alt="photo users"
               />
             </figure>
--- src/services/actions.ts
+++ src/services/actions.ts
@@ -9,6 +9,7 @@ export async function updateProfile(data: FormData, userEmail: string) {
     },
     update: {
       username: data.get("username") as string,
+      avatar: data.get("avatar") as string,
       name: data.get('names') as string,
       subtitle: data.get('subtitle') as string,
       bio: data.get('bio') as string
