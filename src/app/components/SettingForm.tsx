37068695c96c35372ffadbcd8b521bdb40ca4159
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-07-23 17:18:44 -0500

    add api upload files and configure pinata actions whit input to settings form

--- src/app/components/SettingForm.tsx
+++ src/app/components/SettingForm.tsx
@@ -1,13 +1,31 @@
 'use client';
 
 import { Button, TextArea, TextField, } from "@radix-ui/themes";
+import { useEffect, useRef, useState } from "react";
 import { updateProfile } from "@/services/actions";
-import { UploadIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { UploadIcon } from "lucide-react";
 import { Profile } from "@prisma/client";
+import Image from "next/image";
 
 export default function SettingsForms({ userEmail, profile }: { userEmail: string, profile: Profile }) {
   const router = useRouter();
+  const fileInpRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File>();
+  const [avatarUrl, setAvatarUrl] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    if (file) {
+      const data = new FormData();
+      data.set("file", file);
+      fetch("/api/files", { method: "POST", body: data, })
+        .then(res => res.json().then(url => {
+          console.log(url); 
+          setAvatarUrl(url as string)
+        }))
+
+    }
+  }, [file])
 
   return (
     <form action={async (data: FormData) => {
@@ -15,13 +33,33 @@ export default function SettingsForms({ userEmail, profile }: { userEmail: strin
       router.push('/profile');
       router.refresh();
     }}>
-
+      <input type="hidden" name="avatar" value={avatarUrl} />
       <figure className="flex gap-2 items-center">
         <div>
-          <div className="bg-gray-200 size-24 rounded-full"></div>
+          <div className="bg-gray-200 size-24 rounded-full overflow-hidden aspect-square border-2 border-gray-200 shadow-lg">
+            {
+              profile.avatar === null 
+                ? <Image src={avatarUrl || ''} alt="" width={96} height={96} /> 
+                : <Image src={profile.avatar || ''} alt="" width={96} height={96} />
+            }
+          </div>
         </div>
         <div>
-          <Button variant="surface">
+          <input
+            type="file"
+            ref={fileInpRef}
+            className="hidden"
+            onChange={ev => setFile(ev.target.files?.[0])}
+          />
+          <Button
+            variant="surface"
+            onClick={() => {
+              if (fileInpRef.current) {
+                fileInpRef.current.click();
+              }
+            }}
+            type="button"
+          >
             <UploadIcon />
             Change avatar
           </Button>
