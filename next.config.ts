82f5ebde546ec155dcce73c95a995a79f0f44eda
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-05 14:47:41 -0500

    feat: init new project using next js and tailwind css

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos'
-      },
-      {
-        protocol: 'https',
-        hostname: 'silver-cheap-halibut-782.mypinata.cloud'
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/photo-1580489944761-15a19d654956/**',
       }
-    ],
-  },
+    ]
+  }
 };
 
 export default nextConfig;
