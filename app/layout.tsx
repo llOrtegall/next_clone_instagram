6c21debd3facb2f1d443b583ffd7ebbe6474a647
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-06 15:50:19 -0500

    feat: wrap main content in Theme component for improved styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@radix-ui/themes/styles.css";
+import type { Metadata } from "next";
 import "./globals.css";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="h-screen w-full bg-slate-950 text-gray-300 p-4">
-          {children}
-        </main>
+        <Theme>
+          <main className="p-4">
+            {children}
+          </main>
+        </Theme>
       </body>
-    </html>
+    </html >
   );
 }
