37ad98cfe03ea66dfe0f0d2a9a8fbf7b93bd5200
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-09 09:55:34 -0500

    feat: implement newPostCreated function for post creation and redirection

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { redirect } from "next/navigation";
 import { prisma } from "./prisma";
 /**
  * This function updates an existing profile or creates a new one based on the provided user email.
@@ -24,4 +25,23 @@ export async function updateProfileOrCreate(data: FormData, userEmail: string) {
       ...newUserInfo,
     }
   })
+}
+
+export async function newPostCreated(data:FormData, userEmail: string) {
+  const newPostInfo = {
+    authorEmail: userEmail,
+    imageUrl: data.get("imageUrl") as string,
+    description: data.get("description") as string,
+    location: data.get("location") as string,
+  }
+
+  const post = await prisma.post.create({
+    data: newPostInfo
+  })
+
+  if(post.id){
+    return redirect(`/post/${post.id}`);
+  }
+
+  throw new Error('Error creating post');
 }
