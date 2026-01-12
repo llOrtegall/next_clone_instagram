4acb70d9c2d41ceae434d54c3b2a7de5a98157cd
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-12 17:59:37 -0500

    feat: add functions to retrieve post by ID and all posts by user email

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { prisma } from "./prisma";
+
 /**
  * This function updates an existing profile or creates a new one based on the provided user email.
  * @param data FormData containing the profile information
@@ -27,7 +28,13 @@ export async function updateProfileOrCreate(data: FormData, userEmail: string) {
   })
 }
 
-export async function newPostCreated(data:FormData, userEmail: string) {
+/**
+ * Creates a new post with the provided data and user email.
+ * @param data FormData containing the post information
+ * @param userEmail The email of the user creating the post
+ * @returns Promise that resolves to a redirect to the new post's page
+ */
+export async function newPostCreated(data: FormData, userEmail: string) {
   const newPostInfo = {
     authorEmail: userEmail,
     imageUrl: data.get("imageUrl") as string,
@@ -39,9 +46,59 @@ export async function newPostCreated(data:FormData, userEmail: string) {
     data: newPostInfo
   })
 
-  if(post.id){
+  if (post.id) {
     return redirect(`/post/${post.id}`);
   }
 
   throw new Error('Error creating post');
+}
+
+export async function getPostById(id: string) {
+  if (!id) {
+    throw new Error('id is required')
+  }
+
+  try {
+    const post = await prisma.post.findFirst({ where: { id } })
+
+    const map = {
+      id: post?.id,
+      imageUrl: post?.imageUrl,
+      description: post?.description,
+      location: post?.location
+    }
+
+    return map
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message)
+    }
+    console.log(error);
+  }
+
+}
+
+export async function getAllPostByEmail(email: string) {
+  if (!email) {
+    throw new Error('Email is required')
+  }
+
+  try {
+    const posts = await prisma.post.findMany({ where: { authorEmail: email } })
+
+    const map = posts.map(p => {
+      return {
+        id: p?.id,
+        imageUrl: p?.imageUrl,
+      }
+    })
+
+    return map
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message)
+    }
+    console.log(error);
+  }
+
 }
