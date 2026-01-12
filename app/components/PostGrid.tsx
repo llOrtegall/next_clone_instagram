c202cab999ec32fd4b04b69bcc29a37365f3d300
llOrtegall <IvanOrtega_97@hotmail.com>
2026-01-12 17:59:31 -0500

    feat: refactor PostGrid component to fetch and display posts by user email

--- app/components/PostGrid.tsx
+++ app/components/PostGrid.tsx
@@ -1,36 +1,25 @@
+import { getAllPostByEmail } from "@/lib/actions";
 import Image from "next/image"
 
-const images = [
-  'https://picsum.photos/id/30/1024/768',
-  'https://picsum.photos/id/31/768/1024',
-  'https://picsum.photos/id/32/1024/768',
-  'https://picsum.photos/id/33/768/1024',
-  'https://picsum.photos/id/34/1024/768',
-  'https://picsum.photos/id/35/768/1024',
-  'https://picsum.photos/id/36/1024/768',
-  'https://picsum.photos/id/37/768/1024',
-  'https://picsum.photos/id/38/1024/768',
-  'https://picsum.photos/id/39/768/1024',
-  'https://picsum.photos/id/20/1024/768',
-  'https://picsum.photos/id/21/768/1024',
-  'https://picsum.photos/id/22/1024/768',
-  'https://picsum.photos/id/23/768/1024',
-  'https://picsum.photos/id/24/1024/768',
-  'https://picsum.photos/id/25/768/1024',
-  'https://picsum.photos/id/26/1024/768',
-  'https://picsum.photos/id/27/768/1024',
-  'https://picsum.photos/id/28/1024/768',
-  'https://picsum.photos/id/29/768/1024',
-]
+export default async function PostGrid({ email }: { email: string }) {
+    const posts = await getAllPostByEmail(email)
+  
+    if (!posts) {
+      return (
+        <div className="max-w-4xl mx-auto p-6">
+          <h1 className="text-2xl font-bold text-red-600">Post not found</h1>
+          <p className="text-gray-600">The post you are looking for does not exist.</p>
+        </div>
+      );
+    }
 
-export default function PostGrid() {
   return (
     <section className="overflow-y-auto columns-1 sm:columns-2 md:columns-3 2xl:columns-4 gap-4 space-y-4">
       {
-        images.map((src, index) => (
-          <div key={index} className="break-inside-avoid mb-4">
+        posts.map((p) => (
+          <div key={p.id} className="break-inside-avoid mb-4">
             <Image 
-              src={src} 
+              src={p.imageUrl} 
               alt="Post Image" 
               width={300} 
               height={300}
