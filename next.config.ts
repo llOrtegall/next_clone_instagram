2beac758f2c45d792df98623c67597e23f371120
llOrTeGall <IvanOrtega_97@hotmail.com>
2026-01-14 09:19:30 +0000

    feat: update dependencies and change image source for avatars

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: 'avatars.githubusercontent.com',
         port: '',
         pathname: '/**',
       },
