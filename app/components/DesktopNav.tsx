ae191606cb0b0e97e757218294ddfcf5dff30b15
llOrTeGall <IvanOrtega_97@hotmail.com>
2026-01-15 20:19:04 +0000

    feat: refactor DesktopNav component to use MenuArray for dynamic menu rendering and improve styling

--- app/components/DesktopNav.tsx
+++ app/components/DesktopNav.tsx
@@ -1,64 +1,87 @@
-import { CameraIcon, HomeIcon, LayoutGridIcon, SearchIcon, User2Icon } from "lucide-react";
+import { CameraIcon, HomeIcon, LayoutGridIcon, LucideProps, SearchIcon, User2Icon } from "lucide-react";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
+import { Button, Separator } from "@radix-ui/themes";
 import { auth, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@radix-ui/themes";
+
+type IconType = ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>
+
+interface MenuIconProps {
+  path: string,
+  IconRender: IconType,
+  title: string
+}
+
+const MenuArray: MenuIconProps[] = [
+  {
+    path: "/",
+    IconRender: HomeIcon,
+    title: "Home"
+  },
+  {
+    path: "/search",
+    IconRender: SearchIcon,
+    title: "Search"
+  },
+  {
+    path: "/create",
+    IconRender: CameraIcon,
+    title: "Create"
+  },
+  {
+    path: "/browse",
+    IconRender: LayoutGridIcon,
+    title: "Browse"
+  },
+  {
+    path: "/profile",
+    IconRender: User2Icon,
+    title: "Profile"
+  }
+]
+
+const MenuIcon = ({ IconRender, path, title }: MenuIconProps) => {
+  return (
+    <Link
+      className="flex items-center gap-4 px-4 py-3 rounded-xl hover:bg-gray-700 transition-all duration-200 group"
+      href={path}>
+      <IconRender className="w-6 h-6 text-gray-300 group-hover:scale-110 transition-transform" />
+      <p className="text-base font-medium text-gray-300 group-hover:text-white">{title}</p>
+    </Link>
+  )
+}
 
 export default async function DesktopNav() {
   const session = await auth();
 
   return (
-    <aside className="hidden lg:flex lg:flex-col p-6 w-72 border-r border-gray-200 bg-white h-screen sticky top-0">
+    <aside className="hidden lg:flex lg:flex-col p-6 w-72 border-r border-gray-800 h-screen sticky top-0">
 
       <figure className="relative flex items-center justify-start mb-10 mt-4">
-        <Image src={"/iconig.png"} alt="Logo" width={240} height={60} className="object-contain" />
+        <Image src={"/iconig.png"} alt="Logo" width={240} height={60} className="object-contain invert" />
         <p className="absolute top-6 ml-2 text-2xl font-bold bg-linear-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
           Clone
         </p>
       </figure>
 
       <nav className="flex flex-col gap-2 flex-1">
-        <Link
-          className="flex items-center gap-4 px-4 py-3 rounded-xl hover:bg-gray-100 transition-all duration-200 group"
-          href={"/"}>
-          <HomeIcon className="w-6 h-6 text-gray-700 group-hover:scale-110 transition-transform" />
-          <p className="text-base font-medium text-gray-700 group-hover:text-black">Home</p>
-        </Link>
-
-        <Link
-          className="flex items-center gap-4 px-4 py-3 rounded-xl hover:bg-gray-100 transition-all duration-200 group"
-          href={"/search"}>
-          <SearchIcon className="w-6 h-6 text-gray-700 group-hover:scale-110 transition-transform" />
-          <p className="text-base font-medium text-gray-700 group-hover:text-black">Search</p>
-        </Link>
-
-        <Link
-          className="flex items-center gap-4 px-4 py-3 rounded-xl hover:bg-gray-100 transition-all duration-200 group"
-          href={"/create"}>
-          <CameraIcon className="w-6 h-6 text-gray-700 group-hover:scale-110 transition-transform" />
-          <p className="text-base font-medium text-gray-700 group-hover:text-black">Create</p>
-        </Link>
-
-        <Link
-          className="flex items-center gap-4 px-4 py-3 rounded-xl hover:bg-gray-100 transition-all duration-200 group"
-          href={"/browse"}>
-          <LayoutGridIcon className="w-6 h-6 text-gray-700 group-hover:scale-110 transition-transform" />
-          <p className="text-base font-medium text-gray-700 group-hover:text-black">Browse</p>
-        </Link>
-
-        <Link
-          className="flex items-center gap-4 px-4 py-3 rounded-xl hover:bg-gray-100 transition-all duration-200 group"
-          href={"/profile"}>
-          <User2Icon className="w-6 h-6 text-gray-700 group-hover:scale-110 transition-transform" />
-          <p className="text-base font-medium text-gray-700 group-hover:text-black">Profile</p>
-        </Link>
+        {MenuArray.map((menu) => (
+          <MenuIcon
+            key={menu.path}
+            IconRender={menu.IconRender}
+            path={menu.path}
+            title={menu.title} />
+        ))}
       </nav>
 
-      <div className="pt-4 border-t border-gray-200 flex flex-col gap-2 items-center">
+      <Separator size="4" className="my-4" />
+
+      <div className="flex flex-col gap-4 items-center">
         {session && (
           <form action={async () => {
             'use server';
-            await signOut({ redirectTo: '/'});
+            await signOut({ redirectTo: '/' });
           }}>
 
             <Button
